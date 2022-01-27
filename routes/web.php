14b1c71efbc0f5f390a1091669322c323ad07a7f
blum <alfredoblumtorres@gmail.com>
2022-01-27 18:03:38 +0100

    Added web file

--- routes/web.php
+++ routes/web.php
@@ -16,23 +16,8 @@ use App\Http\Controllers\ObjetoController;
 |
 */
 
-<<<<<<< HEAD
-/* Route::get('/', function () {
-    return view('welcome');
-}); */
-
-//Ruta index
-Route::get('/',[ObjetoController::class,'index']);
-
-
-//Ruta editar
-Route::get('/Objetos/form-editar',[ObjetoController::class,'edit']);
-
-//Ruta eliminar
-=======
 //Route::get('/', function () {return view('welcome');});
 
 Route::get('/',[ObjetoController::class,'index'])->name('objetos.index');
 //TO BE CONTONUED
 Route::get('/objetos/create',[ObjetoController::class,'create'])->name('objetos.create');
->>>>>>> 656b5cec1dad0af8e85bc792558f97d5ba20ba7f
